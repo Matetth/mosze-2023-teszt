bb5fc6c03f7eb171fde99891278402612c6f1795
Matetth <mate8588@gmail.com>
2023-09-23 17:56:16 +0200

    Hibák javítása

--- main.cpp
+++ main.cpp
@@ -4,29 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // NELEMENTS az inkább N_ELEMENTS
-    int *b = new int[NELEMENTS];
-    // '' helyett "" kell és ; a végére
-    std::cout << '1-100 ertekek duplazasa'
-    // for ciklus hibásan van megadva
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa";
+    for (int i = 1; i <= N_ELEMENTS; i++)
     {
-    // b[i] helyett b[i-1] legyen
-        b[i] = i * 2;
+        b[i-1] = i * 2;
     }
-    // for ciklus hibásan van megadva
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-    // érték kiiratása hiányzik
-        std::cout << "Ertek:"
+        std::cout << "Ertek: " << b[i] << " ";
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    // , helyett ; kell
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-    // sor végén a ; hiányzik
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
